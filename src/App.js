962c60f38621b7153bd4e78d26063d260268ca81
Prathmesh <prathmeshjadhav380@gmail.com>
2022-12-20 16:42:32 +0530

    react-app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+// function App() {
+//   return "hello";
+// }
+
+function App() {
+  //let mystyle = { color: "white", background: "red" };
+  return (
+    // <div>
+    //   <h1 className="beautify">hello world..!</h1>
+    //   <h1 style={mystyle}>hello universe</h1>
+    //   <h1 style={{ color: "white", background: "purple" }}>hello</h1>
+    // </div>
+
+    <h1 className="beautify">external css styling...</h1>
+  );
+}
+
+export default App;
